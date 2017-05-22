5656865a54063d0876394e0109ac45826e27479b
Josh Rufin <joshrufin@gmail.com>
2017-05-22 13:42:57 -0400

    route setup

--- public/app/app.js
+++ public/app/app.js
@@ -1,1 +1,17 @@
-var app = angular.module('coinMod', []);
+(function(){
+  var app = angular.module('coinMod', ["ngRoute"]);
+
+  app.config(function($routeProvider){
+
+    $routeProvider.when("/",{
+      templateUrl: 'views/dashboard.html'
+    })
+    .when("/info",{
+      templateUrl: 'views/info.html'
+    })
+    .when("/signin",{
+      templateUrl: 'views/signin.html'
+    });
+
+  });
+})();
