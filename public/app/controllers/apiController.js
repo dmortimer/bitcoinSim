9af863c4ad40cf1e8da4ff93954b711267799fd4
Eric Arbour <arboureric@gmail.com>
2017-05-22 11:34:48 -0400

    get price data every 5 second with backup value

--- public/app/controllers/apiController.js
+++ public/app/controllers/apiController.js
@@ -1,5 +1,19 @@
 var app = angular.module('coinMod');
 
-app.controller('apiController', function ($scope, apiFactory) {
-   
+app.controller('apiController', function ($scope, $interval, apiFactory) {
+  $scope.getCurrentPrice = function () {
+    apiFactory.getCurrentPrice().then(function (response) {
+      $scope.currentPrice = response;
+      console.log('CoinDesk BPI real-time:');
+      console.log($scope.currentPrice);
+    });
+  }
+  $scope.getCurrentPrice();
+  $interval(function () {
+    $scope.getCurrentPrice();
+  }, 5000);
+  apiFactory.getHistoricalData().then(function (response) {
+    console.log('CoinDesk historical BPI data:');
+    console.log(response);
+  });
 });
--- public/app/services/apiFactory.js
+++ public/app/services/apiFactory.js
@@ -1,14 +1,30 @@
 var app = angular.module('coinMod');
 
 app.factory('apiFactory', function ($http) {
-  $http.get('http://api.coindesk.com/v1/bpi/currentprice.json').then(function (data) {
-    console.log('CoinDesk BPI real-time:');
-    console.log(data);
-  });
-  $http.get('http://api.coindesk.com/v1/bpi/historical/close.json?start=2010-07-17&end=2017-05-21').then(function (data) {
-    console.log('CoinDesk historical BPI data:');
-    console.log(data);
-  });
   var obj = {};
+  var prevPrice;
+  obj.getCurrentPrice = function () {
+    return $http({
+      method: 'GET',
+      url: 'http://api.coindesk.com/v1/bpi/currentprice.json'
+    }).then(function (response) {
+      prevPrice = response.data.bpi.USD.rate;
+      return response.data.bpi.USD.rate;
+    }).catch(function (error) {
+      console.log(error);
+      return prevPrice;
+    });
+  };
+  obj.getHistoricalData = function () {
+    return $http({
+      method: 'GET',
+      url: 'http://api.coindesk.com/v1/bpi/historical/close.json?start=2010-07-17&end=2017-05-21'
+    }).then(function (response) {
+      return response.data;
+    }).catch(function (error) {
+      console.log(error);
+      return "Dummy Value for Now";
+    });
+  };
   return obj;
 })
