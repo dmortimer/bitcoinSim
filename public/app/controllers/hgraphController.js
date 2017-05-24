fc8daf4a4aeb71460067ae6130ec409a651c8835
Josh Rufin <joshrufin@gmail.com>
2017-05-24 09:09:49 -0400

    graph edited

--- public/app/controllers/hgraphController.js
+++ public/app/controllers/hgraphController.js
@@ -23,7 +23,7 @@ var lineChart = new Chart(ctx, {
     labels: $scope.hdate,
     datasets: [
         {
-            label: "My First dataset",
+            label: "Historical Bitcoin Data",
             fill: false,
             lineTension: 0.1,
             backgroundColor: "rgba(75,192,192,0.4)",
