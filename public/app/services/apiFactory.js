dea51f02cc43fb36e255791b6eebbc9454b350c7
Eric Arbour <arboureric@gmail.com>
2017-05-26 13:57:05 -0400

    some changes based on github confusion

--- public/app/services/apiFactory.js
+++ public/app/services/apiFactory.js
@@ -5,21 +5,13 @@ app.factory('apiFactory', function($http) {
     var obj = {};
     var historicalData;
     var histSubArray = [];
-    var personalHistory = {
-        dates: [],
-        displayDates: [],
-        cash: [],
-        coins: [],
-        historical: [],
-        values: []
-    };
     var currPrice;
-    var testUser = {
+    var user = {
         username: 'testuser',
         password: '12345',
         startingCash: 30000,
         startingCoins: 0,
-        assets: [30000, 0],
+        assets: [],
         accountDate: new Date(2017, 0, 0, 8, 30, 32, 0),
         transactions: [{
                 date: new Date(2017, 0, 3, 8, 30, 32, 0),
@@ -51,101 +43,102 @@ app.factory('apiFactory', function($http) {
                 numCoins: 2,
                 cash: 26832.9
             }
-        ]
+        ],
+        personalHistory: {
+            dates: [],
+            displayDates: [],
+            cash: [],
+            coins: [],
+            historical: [],
+            values: []
+        }
     };
 
     obj.populatePersonalHistory = function() {
-        var firstDate = testUser.accountDate;
+        var firstDate = user.accountDate;
         var lastDate = new Date();
-        //updates 'lastDate' variable to go to yesterday for user account graph because we don't have today's historical data
         var lastDate = new Date(lastDate.setDate(lastDate.getDate() - 2));
         var currDate = firstDate;
-        //generate dates from account creation to yesterday and pushes to personalHistory dates array
+
         while (currDate < lastDate) {
-            personalHistory.dates.push(currDate);
-            //incrementing to the next day
+            user.personalHistory.dates.push(currDate);
             currDate = new Date(currDate.setDate(currDate.getDate() + 1));
-            //cuts string of date object and pushes to display dates array to be used as dates on x axis
-            personalHistory.displayDates.push(currDate.toISOString().substring(0,10));
+            user.personalHistory.displayDates.push(currDate.toISOString().substring(0,10));
         }
-        //loop to populate cash in personal history
         var findi = 0;
-        for (var i = 0; i < personalHistory.dates.length; i++) {
-            if (testUser.transactions.find(function(object) {
-                    return personalHistory.dates[i].getFullYear() === object.date.getFullYear() && personalHistory.dates[i].getMonth() === object.date.getMonth() && personalHistory.dates[i].getDate() === object.date.getDate();
+        for (var i = 0; i < user.personalHistory.dates.length; i++) {
+            if (user.transactions.find(function(object) {
+                    return user.personalHistory.dates[i].getFullYear() === object.date.getFullYear() && user.personalHistory.dates[i].getMonth() === object.date.getMonth() && user.personalHistory.dates[i].getDate() === object.date.getDate();
                 })) {
                 if (i === 0) {
-                    personalHistory.cash.push(testUser.startingCash);
+                    user.personalHistory.cash.push(user.startingCash);
                 } else {
-                    personalHistory.cash.push(testUser.transactions[findi].cash);
+                    user.personalHistory.cash.push(user.transactions[findi].cash);
                     findi++;
                 }
             } else {
                 if (i === 0) {
-                    personalHistory.cash.push(testUser.startingCash);
+                    user.personalHistory.cash.push(user.startingCash);
                 } else {
-                    personalHistory.cash.push(personalHistory.cash[personalHistory.cash.length - 1]);
+                    user.personalHistory.cash.push(user.personalHistory.cash[user.personalHistory.cash.length - 1]);
                 }
             }
         }
-        //loop to populate coins in perosnal history
+
         var findj = 0;
-        for (var i = 0; i < personalHistory.dates.length; i++) {
-            if (testUser.transactions.find(function(object) {
-                    return personalHistory.dates[i].getFullYear() === object.date.getFullYear() && personalHistory.dates[i].getMonth() === object.date.getMonth() && personalHistory.dates[i].getDate() === object.date.getDate();
+
+        for (var i = 0; i < user.personalHistory.dates.length; i++) {
+            if (user.transactions.find(function(object) {
+                    return user.personalHistory.dates[i].getFullYear() === object.date.getFullYear() && user.personalHistory.dates[i].getMonth() === object.date.getMonth() && user.personalHistory.dates[i].getDate() === object.date.getDate();
                 })) {
                 if (i === 0) {
-                    personalHistory.coins.push(testUser.startingCoins);
+                    user.personalHistory.coins.push(user.startingCoins);
                 } else {
-                    personalHistory.coins.push(testUser.transactions[findj].numCoins);
+                    user.personalHistory.coins.push(user.transactions[findj].numCoins);
                     findj++;
                 }
             } else {
                 if (i === 0) {
-                    personalHistory.coins.push(testUser.startingCoins);
+                    user.personalHistory.coins.push(user.startingCoins);
                 } else {
-                    personalHistory.coins.push(personalHistory.coins[personalHistory.coins.length - 1]);
+                    user.personalHistory.coins.push(user.personalHistory.coins[user.personalHistory.coins.length - 1]);
                 }
             }
         }
     };
-    //make api call to database on page load
+
+
     obj.populatePersonalHistory();
-    //return asset values
     obj.getCurrentAssets = function() {
-        return testUser.assets;
+        return user.assets;
     };
-    //return display dates for user account graph
     obj.getPersonalHistoryDates = function() {
-        return personalHistory.displayDates;
+        return user.personalHistory.displayDates;
     };
-    //return total account values for user account graph
     obj.getPersonalHistoryValues = function() {
-        return personalHistory.values;
+        return user.personalHistory.values;
     };
-    //function to update coins and assets when coin is bought and log the transaction
     obj.buyCoin = function(numBuy) {
-        testUser.assets[0] -= (currPrice * numBuy);
-        testUser.assets[1] += numBuy;
-        testUser.transactions.push({
+        user.assets[0] -= (currPrice * numBuy);
+        user.assets[1] += numBuy;
+        user.transactions.push({
             date: new Date(),
-            type: "buy",
-            numCoins: numBuy
+            coinChange: numBuy,
+            numCoins: 2,
+            cash: 26832.9
         });
-        return testUser.assets;
+        return user.assets;
     };
-    //function to update coins and assets when coin is sold and log the transaction
     obj.sellCoin = function(numSell) {
-        testUser.assets[0] += (currPrice * numSell);
-        testUser.assets[1] -= numSell;
-        testUser.transactions.push({
+        user.assets[0] += (currPrice * numSell);
+        user.assets[1] -= numSell;
+        user.transactions.push({
             date: new Date(),
             type: "sell",
             numCoins: numSell
         });
-        return testUser.assets;
+        return user.assets;
     };
-    //external api request to get current price of bitcoins by the minute
     obj.getCurrentPrice = function() {
         return $http({
             method: 'GET',
@@ -158,30 +151,27 @@ app.factory('apiFactory', function($http) {
             return currPrice;
         });
     };
-    //pulls historical data from database
     obj.getHistoricalData = function() {
         return $http({
             method: 'GET',
             url: '/api/history'
         }).then(function(response) {
-            //turns dates from database into JavaScript date format
             response.data.forEach(function(item) {
                 item.price_date = new Date(item.price_date);
             });
-            //stores database response into historcalData variable
             historicalData = response.data;
-            //finds the day the user created the account to the current date and selects those dates
-            histSubArray = historicalData.slice(historicalData.findIndex(function(object){
-                return testUser.accountDate.getFullYear() === object.price_date.getFullYear() && testUser.accountDate.getMonth() === object.price_date.getMonth() && testUser.accountDate.getDate() === object.price_date.getDate();
+            histSubArray= historicalData.slice(historicalData.findIndex(function(object){
+
+                return user.accountDate.getFullYear() === object.price_date.getFullYear() && user.accountDate.getMonth() === object.price_date.getMonth() && user.accountDate.getDate() === object.price_date.getDate();
             }));
-            //populate personal History array with historical prices for past calculations
             histSubArray.forEach(function(object){
-                personalHistory.historical.push(object.price)
+                user.personalHistory.historical.push(object.price)
             });
-            //actually calculate historical account values based on historical prices, number of coins held in the past, and cash in account in the past and push those to the personal history values array
-            for(var i = 0; i < personalHistory.dates.length; i++){
-                personalHistory.values.push((personalHistory.coins[i] * personalHistory.historical[i]) + personalHistory.cash[i])
+            for(var i = 0; i < user.personalHistory.dates.length; i++){
+                user.personalHistory.values.push((user.personalHistory.coins[i] * user.personalHistory.historical[i]) + user.personalHistory.cash[i])
             };
+            console.log(user.personalHistory);
+
             return response.data;
         }).catch(function(error) {
             console.log(error);
