1a9ce71adf6b3837ac7729d2a5d132bcf0a280dc
Eric Arbour <arboureric@gmail.com>
2017-05-28 13:30:39 -0400

    updated sql queries

--- ref/sql-ref.sql
+++ ref/sql-ref.sql
@@ -2509,3 +2509,4 @@ INSERT INTO coinData (price_date, price) VALUES('2017-05-23', 2291.4775);
 INSERT INTO coinData (price_date, price) VALUES('2017-05-24', 2476.2963);
 INSERT INTO coinData (price_date, price) VALUES('2017-05-25', 2357.5038);
 INSERT INTO coinData (price_date, price) VALUES('2017-05-26', 2247.4825);
+INSERT INTO coinData (price_date, price) VALUES('2017-05-27', 2106.3075);
