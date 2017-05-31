d42570cd5c98d9ed80a799ed6089c9ebeb0eb1d6
Eric Arbour <arboureric@gmail.com>
2017-05-31 11:49:24 -0400

    user login and interactive table working

--- public/app/app.js
+++ public/app/app.js
@@ -1,1 +1,9 @@
 var app = angular.module('coinMod', ["ui.router", "ui.bootstrap"]);
+
+app.directive('transxnTable', function() {
+  return {
+    restrict: 'E',
+    replace: false,
+    templateUrl:'views/trxnTable.html'
+  }
+})
